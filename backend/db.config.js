22b1fc6c63f3b20cb84c8bc0588a32ca13f5d7b7
kdn95 <khangdn95@gmail.com>
2024-09-20 05:52:00 +0000

    added config, postgres setup, server.js update for main route

---
+++ backend/db.config.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const { Pool } = require("pg");
+
+const database = process.env.PGDATABASE;
+
+const connectionString = 'postgres://postgres:circleshare@localhost:5432/database'
+
+const pool = new Pool({
+  connectionString: connectionString,
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+  end: () => pool.end(),
+};
--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,22 @@
+require('dotenv').config({ path: __dirname + '/.env' });
 const express = require('express');
+const pool = require(__dirname + '/db.config.js');
+
 const app = express();
-const PORT = process.env.PORT || 5002;
-app.get('/', (req, res) => res.send('Hello from the backend!'));
+const PORT = process.env.PORT || 5003;
+
+// Function to handle health check (fetch Categories)
+const getCategories = (req, res) => {
+  pool.query('SELECT * FROM Categories', (error, categories) => {
+    if (error) {
+      return res.status(500).json({ error: 'Database query failed' });
+    }
+    res.status(200).json(categories.rows);
+  });
+};
+
+// Route to get Categories data
+app.get('/', getCategories);
+
+// Start server
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
