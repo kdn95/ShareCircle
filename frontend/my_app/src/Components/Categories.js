812625a5abe7c1f16b28bbac9668e47b89e0a009
Carole <c.zenruffinen@hotmail.com>
2024-09-26 03:54:47 +0000

    changes to database, added pic for electronics, changed categories to show pic

--- frontend/my_app/src/Components/Categories.js
+++ frontend/my_app/src/Components/Categories.js
@@ -31,18 +31,17 @@ const Categories = () => {
     <h1 className="categories-title">Categories</h1>
     <div className="categories-container">
       {categories.map((category) => (
-        <Card sx={{ maxWidth: 345, margin: '20px' }} key={category.ID} className="category-item"> {/* Unique key */}
+        <Card sx={{ maxWidth: 345, margin: '20px' }} key={category.id} className="category-item">
           <CardActionArea>
             <CardMedia
               component="img"
               className="category-image"
               height="140"
-              image={category.ImageUrl || '/images/fitness.jpg'} // Fallback image if none is available
-              alt={category.Name} // Alt text for the image
+              image={category.Category_pic || '/static/images/cards/default.jpg'} // Fallback image
+              alt={category.Name}
             />
             <CardContent>
-              {/* Category Name */}
-              <h3 ClassName="category-name">{category.Name}</h3>
+              <h3>{category.Name}</h3>
             </CardContent>
             </Link>
           </CardActionArea>
