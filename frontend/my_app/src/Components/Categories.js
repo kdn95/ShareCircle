9c12f62c74bb73356b5e485739d83de1008778da
nickelannG <nicoleanngorospe@gmail.com>
2024-09-26 05:23:51 +0000

    log in button in accounts

--- frontend/my_app/src/Components/Categories.js
+++ frontend/my_app/src/Components/Categories.js
@@ -30,10 +30,11 @@ const Categories = () => {
     <h1 className="categories-title">Categories</h1>
     <div className="categories-container">
       {categories.map((category) => (
-        <Card sx={{ maxWidth: 345, margin: '20px' }} key={category.id} className="category-item">
+        <Card sx={{ maxWidth: 345, margin: '20px' }} key={category.ID} className="category-item">
           <CardActionArea>
             <CardMedia
               component="img"
+              className="category-image"
               height="140"
               image={category.Category_pic || '/static/images/cards/default.jpg'} // Fallback image
               alt={category.Name}
