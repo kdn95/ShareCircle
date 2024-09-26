94f58f4d237c45068142f6f93eb3b25e8cdba2c8
nickelannG <nicoleanngorospe@gmail.com>
2024-09-26 05:23:51 +0000

    log in button in accounts

--- frontend/my_app/src/Components/Categories.js
+++ frontend/my_app/src/Components/Categories.js
@@ -34,6 +34,7 @@ const Categories = () => {
           <CardActionArea>
             <CardMedia
               component="img"
+              className="category-image"
               height="140"
               image={category.Category_pic || '/static/images/cards/default.jpg'} // Fallback image
               alt={category.Name}
