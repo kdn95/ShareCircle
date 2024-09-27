3c7be2f73e996436634788c5ac788a4994d792f0
kdn95 <khangdn95@gmail.com>
2024-09-27 04:41:57 +0000

    items nearby works

--- frontend/my_app/src/App.js
+++ frontend/my_app/src/App.js
@@ -1,41 +1,27 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { useAuth0 } from '@auth0/auth0-react';
-import Categories from './Components/Categories';
 import Navbar from './Components/Navbar';
-import Auth from './Components/Auth'; // Renamed to Auth
 import NearbyItems from './Components/NearbyItems';
 import Home from './Components/Home';
 import './index.css';
 
 const App = () => {
-  const { loginWithRedirect, isAuthenticated } = useAuth0(); // Get login and auth state from Auth0
-
-  // Function to handle account click
+  const { loginWithRedirect } = useAuth0();
+  
   const handleAccountClick = () => {
-    if (!isAuthenticated) {
-      loginWithRedirect(); // Redirect to login if not authenticated
-    } else {
-      console.log('Account clicked');
-    }
+    loginWithRedirect();
   };
+
 
   return (
     <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home 
-            isAuthenticated={isAuthenticated} 
-            user={user} 
-            loginWithRedirect={loginWithRedirect} 
-            logout={logout} 
-          />}
-          />
-        <Route path="/items/nearby" element={<NearbyItems />} />
-      </Routes>
-      <Categories /> {/* Render the Categories component */}
       <Navbar onAccountClick={handleAccountClick} /> {/* Pass the function to Navbar */}
-    </div>
+      <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/items/nearby" element={<NearbyItems />} />
+    </Routes>
+  </Router>
   );
 };
 
