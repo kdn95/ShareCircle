035e41ff4dc4dd11ea9cec97d6aa9db73ea7209d
nickelannG <nicoleanngorospe@gmail.com>
2024-09-27 02:34:04 +0000

    log in from accounts

--- frontend/my_app/src/App.js
+++ frontend/my_app/src/App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import Categories from './Components/Categories';
 import Navbar from './Components/Navbar';
 import Auth from './Components/Auth'; // Renamed to Auth
+import { useAuth0 } from '@auth0/auth0-react'; // Import Auth0 hook
 import './index.css';
 
 const App = () => {
+  const { loginWithRedirect, isAuthenticated } = useAuth0(); // Get login and auth state from Auth0
 
-  // Function to handle account click, passed to Navbar
+  // Function to handle account click
   const handleAccountClick = () => {
-    console.log('Account clicked');
+    if (!isAuthenticated) {
+      loginWithRedirect(); // Redirect to login if not authenticated
+    } else {
+      console.log('Account clicked');
+    }
   };
 
   return (
