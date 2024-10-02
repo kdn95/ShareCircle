287db9e64e193bc5c53cf87961927f973de62eed
Carole <c.zenruffinen@hotmail.com>
2024-10-02 07:05:53 +0000

    chnaged db config file

--- backend/db.config.js
+++ backend/db.config.js
@@ -5,13 +5,10 @@ const { Pool } = require("pg");
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({
   connectionString: connectionString,
-  ssl: {
-    rejectUnauthorized: false // Set this if your database requires SSL
-  }
 });
 
-// Export query and end methods for database interactions
 module.exports = {
   query: (text, params) => pool.query(text, params),
   end: () => pool.end(),
 };
+
