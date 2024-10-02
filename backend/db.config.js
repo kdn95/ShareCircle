cc02fede480c17fdc1f709aa04b3ba9bdf57e430
Carole <c.zenruffinen@hotmail.com>
2024-10-02 07:05:53 +0000

    chnaged db config file

--- backend/db.config.js
+++ backend/db.config.js
@@ -1,21 +1,16 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-// Determine the connection string based on the environment
-const connectionString =
-  process.env.DB_URL || // Use the connection string from the environment variable
-  'postgres://postgres:circleshare@localhost:5432/postgres'; // Fallback to local connection
+const database = process.env.PGDATABASE;
 
-// Create a new pool instance with the connection string
+const connectionString = 'postgres://postgres:circleshare@localhost:5432/postgres'
+
 const pool = new Pool({
   connectionString: connectionString,
-  ssl: {
-    rejectUnauthorized: false // Set this if your database requires SSL
-  }
 });
 
-// Export query and end methods for database interactions
 module.exports = {
   query: (text, params) => pool.query(text, params),
   end: () => pool.end(),
 };
+
