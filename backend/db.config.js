cf8da79332b8d3c5a0e48ea2cf50ee419af189c5
Carole <c.zenruffinen@hotmail.com>
2024-10-02 06:46:50 +0000

    changes to make render work

--- backend/db.config.js
+++ backend/db.config.js
@@ -1,17 +1,20 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const database = process.env.PGDATABASE;
+// Determine the connection string based on the environment
+const connectionString =
+  process.env.DB_URL || // Use the connection string from the environment variable
+  'postgres://postgres:circleshare@localhost:5432/postgres'; // Fallback to local connection
 
-const connectionString = 'postgres://postgres:circleshare@localhost:5432/postgres'
-
+// Create a new pool instance with the connection string
 const pool = new Pool({
   connectionString: connectionString,
   ssl: {
-    rejectUnauthorized: false // Ensure to set this if your database requires SSL
+    rejectUnauthorized: false // Set this if your database requires SSL
   }
 });
 
+// Export query and end methods for database interactions
 module.exports = {
   query: (text, params) => pool.query(text, params),
   end: () => pool.end(),
