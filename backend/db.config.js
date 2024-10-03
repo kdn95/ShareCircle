e010d6c8317543679cb242295cfc170ec1a8871a
nickelannG <nicoleanngorospe@gmail.com>
2024-10-03 05:47:29 +0000

    changes to render db

--- backend/db.config.js
+++ backend/db.config.js
@@ -2,7 +2,8 @@ require("dotenv").config();
 const { Pool } = require("pg");
 
 // Directly using your Render database details in the connection string
-const connectionString = process.env.DATABASE_URL;
+const connectionString = 'postgres://db_xx98_user:WtufEwvyo3n9gVIXQZC69GRNlsEq6meH@dpg-cruuss5ds78s73a5841g-a.oregon-postgres.render.com:5432/db_xx98';
+
 const pool = new Pool({
   connectionString: connectionString,
   ssl: {
