4f7031d72d67b0d70c0abcb51d6f6697c123be02
nickelannG <nicoleanngorospe@gmail.com>
2024-10-03 11:19:17 +0000

    added loader to categoryItems

--- frontend/my_app/src/Components/CategoryItems.js
+++ frontend/my_app/src/Components/CategoryItems.js
@@ -51,11 +51,11 @@ const CategoryItems = () => {
   return (
     <div className="Category-items-container">
       <div className="Category-items-title-address-container">
-      <h1 className="Category-items-title">Items in {category_name}</h1>
-      {userAddress && (
-      <p className="user-address">
-        {userAddress.street}, {userAddress.city}, {userAddress.state} {userAddress.postcode}
-      </p>
+        <h1 className="Category-items-title">Items in {category_name}</h1>
+        {userAddress && (
+          <p className="user-address">
+            {userAddress.street}, {userAddress.city}, {userAddress.state} {userAddress.postcode}
+          </p>
         )}{/* Display user's address */}
       </div>
       <div className="items-container">
