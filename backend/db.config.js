104ee04402180bc1193d4cbbc6f7c997c107cecf
nickelannG <nicoleanngorospe@gmail.com>
2024-10-03 05:55:01 +0000

    ssl

--- backend/db.config.js
+++ backend/db.config.js
@@ -5,6 +5,9 @@ const { Pool } = require("pg");
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({
   connectionString: connectionString,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = {
