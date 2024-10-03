58fb534216832e82cd0bbf2d0ec6afbbe282aaf9
Carole <c.zenruffinen@hotmail.com>
2024-10-03 01:31:24 +0000

    back to status quo

--- backend/db.config.js
+++ backend/db.config.js
@@ -4,10 +4,7 @@ const { Pool } = require("pg");
 // Directly using your Render database details in the connection string
 const connectionString = process.env.DATABASE_URL;
 const pool = new Pool({
-  connectionString: process.env.DB_URL,
-  ssl: {
-    rejectUnauthorized: false, // Set this to false if you're using self-signed certificates
-  },
+  connectionString: connectionString,
 });
 
 module.exports = {
