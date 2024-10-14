53310f8250360bb3caf59dfa4aec7eac5f401fec
nickelannG <nicoleanngorospe@gmail.com>
2024-10-14 12:04:18 +0000

    profile page styling

--- frontend/my_app/src/Components/Profile.js
+++ frontend/my_app/src/Components/Profile.js
@@ -54,6 +54,7 @@ const Profile = () => {
         <div className="profile-image-container">
           <img src={user.picture}  alt="User Profile" className="profile-image" />
         </div>
+        {/* Moved user name outside profile-image-container */}
         <h2 className="user-name">{user.name || 'User Name'}</h2>
       </div>
       <div className="profile-info">
