7727bb49204a1f867d2f32ce9eda00b4e2e8078f
kdn95 <khangdn95@gmail.com>
2024-10-21 01:00:42 +0000

    updated itemform examples

--- frontend/my_app/src/Components/ItemForm.js
+++ frontend/my_app/src/Components/ItemForm.js
@@ -57,7 +57,7 @@ const ItemForm = () => {
     <form onSubmit={handleSubmit} className='add-item-form'>
       <input
         type="text" id='item-name-field'
-        placeholder="Item Name e.g. Drone Zero X"
+        placeholder="Item Name"
         value={itemName}
         onChange={(e) => setItemName(e.target.value)}
         required
